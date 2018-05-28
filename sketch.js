f36a5a7e716e6ba08e27ed2ab954891167fb4d74
Florian Mayr <florianmayr@Florians-MacBook-Pro.local>
2018-05-28 21:14:37 +0200

    juhu

--- sketch.js
+++ sketch.js
@@ -1,37 +1,42 @@
 var data;
-var targetYear = 1950;
+var targetYear = 42;
 
 function preload(){
-  data = loadJSON("data.json");
+  data = loadJSON("data2.json");
 }
 
 function setup() {
   noLoop();
-  createCanvas(640, 360);
+  createCanvas(900, 600);
+  background(200);
 
-  if (window.location.hash != "") {
-    targetYear = window.location.hash.substring(1);
-  }
-  console.log(targetYear);
+  // if (window.location.hash != "") {
+  //   targetYear = window.location.hash.substring(1);
+  // }
+  // console.log(targetYear);
 }
 
 function draw() {
+  	translate(300, 300);
   for(var index = 0; index < data.houses.length; index++) {
     var obj = data.houses[index];
 
-    if (obj.year == targetYear) {
-      if (obj.area == "Sonnwiese") {
-      fill(0)
-      }
+    if (obj.erbaut >= targetYear) {
+      // if (obj.area == "Sonnwiese") {
+      // fill(0)
+      // }
+      fill(0);
 
       beginShape();
       for(var vIndex = 0; vIndex < obj.vertices.length; vIndex++ ) {
-        var vertex = obj.vertices[vIndex];
+        var knotenpunkte = obj.vertices[vIndex];
 
-        var x = vertex[0];
-        var y = vertex[1];
+        var x = knotenpunkte[0];
+        var y = knotenpunkte[1];
 
-        curveVertex(x, y);
+        console.log(obj.vertices);
+        console.log(obj.Adresse);
+        vertex(x, y);
       }
       endShape(CLOSE);
     }
