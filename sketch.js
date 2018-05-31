b598ea6a677dd28e038d6a237443f72729a4d2ab
Florian Mayr <florianmayr@florians-mbp.home>
2018-05-31 14:26:14 +0200

    geht

--- sketch.js
+++ sketch.js
@@ -1,60 +1,72 @@
-$('input').popup(); 
-
 var data;
-var targetYear = 42;
+//var targetYear = 42;
+var jahr = 666;
+var gui;
 
 function preload(){
   data = loadJSON("data2.json");
 }
 
 function setup() {
-//  noLoop();
-  createCanvas(800, 600);
-  tSlider = createSlider(0, 2018, 1992);
-  tSlider.position(20, 20);
+  createCanvas(800, 600, SVG);
 
+//  myGui = createGui("settings");
+  // myGui.addGlobals("move");
 
+//  myGui.addGlobals("jahr");
+  //sliderRange(0, 2018);
 
-  // if (window.location.hash != "") {
-  //   targetYear = window.location.hash.substring(1);
-  // }
-  // console.log(targetYear);
+//  myGui.addButton("speichern", function() {
+//      save();
+//  });
+gui = createGui('p5.gui');
+
+sliderRange(0, 3000, 1);
+gui.addGlobals('jahr');
+
+// Only call draw when then gui is changed
+noLoop();
 }
 
 function draw() {
+  background(240);
+
   translate(400, 350);
   scale(1.5);
-
-  var targetYear = tSlider.value();
-    background(200);
-    textSize(32);
-    fill(0);
-    text(targetYear, 10, 90);
-
-    domtext = createP(targetYear);
-    domtext.position(40, 40);
+//  jahr = targetYear;
+  var targetYear = jahr;
+//  background(240);
+  textSize(32);
+  fill(0);
+  text(jahr, 10, 90);
 
   for(var index = 0; index < data.houses.length; index++) {
     var obj = data.houses[index];
 
     if (obj.erbaut == targetYear) {
-      // if (obj.area == "Sonnwiese") {
-      // fill(0)
-      // }
-      fill(0);
-
+      fill(200);
+      noStroke();
       beginShape();
       for(var vIndex = 0; vIndex < obj.vertices.length; vIndex++ ) {
         var knotenpunkte = obj.vertices[vIndex];
-
         var x = knotenpunkte[0];
         var y = knotenpunkte[1];
-
-        console.log(obj.vertices);
-        console.log(obj.Adresse);
         vertex(x, y);
       }
       endShape(CLOSE);
+
+      // fill(0);
+      // var anzahl = obj.Bewohner;
+      // textSize(10);
+      // text(anzahl, x, y);
+
+//      noLoop();
+
+
     }
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
